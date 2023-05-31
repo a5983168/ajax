5cf05b43f5ac59ba0de7b3669ee942632b464962
mayday0209 <673766077@qq.com>
2023-05-31 17:39:52 +0800

    新增代码

---
+++ public/main.js
@@ -0,0 +1,66 @@
+getJSON.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("get", "/5.json");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(typeof request.response);
+      console.log(request.response);
+      const bool = JSON.parse(request.response);
+      console.log(typeof bool);
+      console.log(bool);
+    }
+  };
+  request.send();
+};
+getXML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/4.xml");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const dom = request.responseXML;
+      const text = dom.getElementsByTagName("warning")[0].textContent;
+      console.log(text.trim());
+    }
+  };
+  request.send();
+};
+getHTML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/3.htm");
+  request.onload = () => {
+    const div = document.createElement("div");
+    div.innerHTML = request.response;
+    document.body.appendChild(div);
+  };
+  request.onerror = () => {};
+  request.send();
+};
+getJS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/2.js");
+  request.onload = () => {
+    const script = document.createElement("script");
+    script.innerHTML = request.response;
+    document.body.appendChild(script);
+  };
+  request.onerror = () => {};
+  request.send();
+};
+
+getCSS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/style.css"); 
+  request.onreadystatechange = () => {
+    console.log(request.readyState);
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const style = document.createElement("style");
+        style.innerHTML = request.response;
+        document.head.appendChild(style);
+      } else {
+        alert("加载 CSS 失败");
+      }
+    }
+  };
+  request.send(); 
+};
